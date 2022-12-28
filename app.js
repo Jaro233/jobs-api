ee07891ed51537e738e345f70eff4a1262478537
Jaro233 <jaroslaw.waliczek@vp.pl>
2022-12-28 16:27:05 +0100

    added swagger docs

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
 
+//swagger
+const swaggerUI = require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDocument = YAML.load("./swagger.yaml")
+
 const express = require('express');
 const app = express();
 
@@ -36,10 +41,13 @@ app.use(helmet());
 app.use(cors())
 app.use(xss())
 
-// routes
+
 app.get('/', (req,res) => {
-  res.send('jobs api')
+  res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</a>')
 })
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
+
+// routes
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticeUser, jobsRouter)
 
